badb7da05f20b1ef4355a8da78247a30c0b5c3c5
RafsanFaiyad <rowanrafsan@gmail.com>
2026-07-29 18:04:42 +0600

    Implemented function to count the vowels

--- String_prossecor.py
+++ String_prossecor.py
@@ -1,2 +1,10 @@
 def reverse_string(text):
     return text[::-1]
+def count_vowels(text):
+    vowels = "aeiouAEIOU"
+    count = 0
+
+    for char in text:
+        if char in vowels:
+            count += 1
+    return count
